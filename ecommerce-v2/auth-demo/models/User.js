58f0c2438d1c5b79b605b038d81c707c974e7037
rishika570 <rishikasingh570@gmail.com>
2023-04-05 15:28:13 +0530

    commit

---
+++ ecommerce-v2/auth-demo/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = new mongoose.Schema({
+
+    username: {
+
+        type: String,
+        trim: true
+    },
+
+    hash : {
+
+        type: String,
+        trim: true
+    }
+})
+
+const User = mongoose.model("User", userSchema)
+
+module.exports = User
